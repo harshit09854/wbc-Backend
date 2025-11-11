d730df1395aa28f51f9f04baaaa99493292d965b
harshit <Harshitpokharna24@gmail.com>
2025-11-11 12:22:18 +0530

    hi

--- controllers/productController.js
+++ controllers/productController.js
@@ -184,7 +184,8 @@ const deleteProduct = async (req, res) => {
   console.log(sellerId)
   try {
     const products = await productModel.find({ seller: sellerId });
-    if (products.length === 0) {x
+    console.log(products)
+    if (products.length === 0) {
       return res
         .status(404)
         .json({ message: "No products found for this seller" });
